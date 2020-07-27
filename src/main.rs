3f4ec070b593d289b091b9de1508c6e56327e8bb
pv <parshav@live.com>
2020-07-27 10:42:52 -0500

    Add working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::env;
 
+fn main() {
+    let mut add: f64 = 0.0;
+    env::args().into_iter()
+        .skip(1)
+        .map(|num| { num.parse::<f64>().expect("failed to parse") })
+        .for_each(|f| {
+            add += f;
+        });
+    println!("Addition : {}", add);
 }
